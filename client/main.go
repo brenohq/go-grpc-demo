e7dc8e6fa94cf092dcc612e9ce2febb9f0038b5a
brenohq <brenosc2@hotmail.com>
2024-01-16 18:17:49 -0300

    first client impl

---
+++ client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/brenohq/go-grpc-demo/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8081"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	callSayHello(client)
+}
